44b2455eb59b37fe4ce1f2f3d646eda534222929
Cameron Labelle <labelle.cam3@gmail.com>
2026-05-18 22:36:05 -0400

    Maybe it will work now

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+﻿import {Box} from "@mui/material";
+
+
+export function Footer() {
+    
+    return (
+        <Box sx={{
+            width: "100%",
+            height: "200px",
+            bgcolor: "primary.main",
+            color: "white",
+        }}>
+            
+        </Box>
+    )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/{SEG3125-Portfolio}",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
